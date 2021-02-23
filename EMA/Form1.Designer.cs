be610c9424effa974389b084e818f59ab94c67a2
bluesnooker123 <dev333perfect@gmail.com>
2021-02-23 19:24:30 +0700

    Initial Commit

---
+++ EMA/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace EMA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btn_start.Location = new System.Drawing.Point(65, 71);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(147, 65);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btn_start);
+            this.Name = "Form1";
+            this.Text = "EMA";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+    }
+}
+
---
+++ EMA/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace EMA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            string targetDirectory = "../Output/Original";
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string filePath in fileEntries)
+                ProcessFile(filePath);
+
+            //string fileName = "../Output/Original/CSV-WES.csv";
+            //ProcessFile(fileName);
+        }
+
+        private void ProcessFile(string filePath)
+        {
+            string fileName = filePath.Substring(filePath.IndexOf('\\') + 1);
+            string out_path = "../Output/Adjusted/" + fileName;
+                        
+            string[] org_lines = System.IO.File.ReadAllLines(filePath);
+
+            StringBuilder intermediate_result_1 = Remove_0_blank(org_lines);
+
+            string seperater = "\r\n";
+            string[] temp_stringArray = intermediate_result_1.ToString().Split(seperater.ToCharArray(), StringSplitOptions.RemoveEmptyEntries).ToArray();
+            StringBuilder intermediate_result_2 = Remove_duplicated_weekend_val(temp_stringArray);
+
+
+
+            System.IO.File.WriteAllText(out_path, intermediate_result_2.ToString());
+
+
+            MessageBox.Show("Save to \"" + out_path + "\" successfully!", "success");
+
+        }
+        private bool check_exist_0_blank(string line)
+        {
+            string[] columns = line.Split(',');
+            foreach (string column in columns)
+            {
+                if (string.Compare(column,"0") == 0)
+                    return false;
+                if (string.Compare(column, "") == 0)
+                    return false;
+            }
+            return true;
+        }
+        private StringBuilder Remove_0_blank(string[] lines)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (string line in lines)
+            {
+                bool flag = check_exist_0_blank(line);
+                if (flag)
+                    result.AppendLine(line);
+            }
+            return result;
+        }
+
+        private StringBuilder Remove_duplicated_weekend_val(string[] stringArray)
+        {
+            StringBuilder result = new StringBuilder();
+            var lines = new HashSet<int>();
+            foreach (string line in stringArray)
+            {
+                string[] temp_arr = line.Split(',').ToArray();
+                int hc = temp_arr[1].GetHashCode();
+                if (lines.Contains(hc))
+                    continue;
+
+                if (string.Compare(temp_arr[1],"Date")!=0)
+                {
+                    DateTime date = Convert.ToDateTime(temp_arr[1]);
+                    DayOfWeek day = date.DayOfWeek;
+                    if ((day == DayOfWeek.Saturday) || (day == DayOfWeek.Sunday))
+                        continue;
+                }
+
+                lines.Add(hc);
+                result.AppendLine(line);
+            }
+            return result;
+        }
+    }
+}
